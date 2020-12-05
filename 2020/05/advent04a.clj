b0400b689e724b3b04489752eb92f4fa91f1acb5
George Louthan <duplico@dupli.co>
2020-12-05 16:36:42 -0600

    Add day 4 part 1 and input. Clojure!

---
+++ 2020/05/advent04a.clj
@@ -0,0 +1,20 @@
+(ns advent04a
+  (:require [clojure.string :as str] clojure.java.io))
+
+(defn row-col-number [min max spec]
+  (if (= min max)
+    min
+    (let [mid (quot (+ max min) 2)]
+      (if (or (str/starts-with? spec "F") (str/starts-with? spec "L")) ; TODO: error handling
+        (recur min mid (subs spec 1)) ; Front or Left
+        (recur (+ mid 1) max (subs spec 1))))))
+
+(defn seat-id [spec]
+    (+ (* 8 (row-col-number 0 127 (subs spec 0 7)))
+       (row-col-number 0 7 (subs spec 7))))
+
+(defn highest-seat-id [input-file-path]
+  (with-open [input (clojure.java.io/reader input-file-path)]
+    (reduce max (map seat-id (line-seq input)))))
+
+(println (highest-seat-id "input.txt"))
