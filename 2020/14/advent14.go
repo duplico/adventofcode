21366dcb05bbd75bdf3876bbc42da7aa4acc2c3f
George Louthan <duplico@dupli.co>
2020-12-15 23:02:13 -0600

    Convert today's setup to a single file.

--- 2020/14/advent14.go
+++ 2020/14/advent14.go
@@ -10,23 +10,12 @@ import (
 	"strings"
 )
 
-func main() {
-	fmt.Println("Hello world!")
+var maskBitmask uint64 = 0x00
+var maskValue uint64 = 0x00
+var mem = make(map[uint64]uint64)
 
-	// memory := make(map[uint]uint64)
-	var maskBitmask uint64 = 0x00
-	var maskValue uint64 = 0x00
-	var mem = make(map[uint64]uint64)
-
-	file, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
+func partOne(scanner *bufio.Scanner) {
 	cmdRexp := regexp.MustCompile(`^(?P<var>mask|mem)(\[(?P<addr>\d+)\])? = (?P<value>[\dX]+)$`)
-
-	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		match := cmdRexp.FindStringSubmatch(line)
@@ -64,6 +53,39 @@ func main() {
 	}
 
 	fmt.Printf("Total memory values: %d", memTotal)
+}
+
+func partTwo(scanner *bufio.Scanner) {
+
+}
+
+func main() {
+
+	inputFile := "sample_input.txt"
+
+	if len(os.Args) > 3 || len(os.Args) == 1 {
+		panic("Expected: advent14 {1|2} [input.txt]")
+	} else if len(os.Args) == 3 {
+		inputFile = os.Args[2]
+	}
+	adventPart := os.Args[1]
+
+	file, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	switch adventPart {
+	case "1":
+		partOne(scanner)
+	case "2":
+		partTwo(scanner)
+	default:
+		panic("No such part for day 14!")
+	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
