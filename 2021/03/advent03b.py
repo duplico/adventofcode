9b8721bf186deefc5b1990e3cea67d5cfe314a7e
George Louthan <duplico@dupli.co>
2021-12-03 13:04:14 -0600

    Add working Python solution for day 3 part 2.

--- 2021/03/advent03b.py
+++ 2021/03/advent03b.py
@@ -1,44 +1,60 @@
 import fileinput
 from os.path import commonprefix
+import operator
+
+def filter_prefix(candidates: set[str], prefix: str = '', ones_operator=operator.ge) -> int:
+    """Filter a set of binary number strings by prefix, returning a single int.
+
+    This function considers a set of candidate strings, which must encode
+    valid binary numbers. They are iteratively filtered by prefix, selecting
+    each bit by whether it is the more common or less common place value for
+    each successive bit (MSB first), until only a single value remains with
+    that prefix, which is returned as an int.
+
+    Specific behavior is determined by the ones_operator parameter:
+    
+    `ge`: Select the more common value for prefix, ties resolved to '1'
+    `gt`: Select the more common value for prefix, ties resolved to '0'
+    `le`: Select the less common value for prefix, ties resolved to '1'
+    `lt`: Select the less common value for prefix, ties resolved to '0'
+
+    """
+    # Base case; when we're down to just one option, pop it and
+    #  return it as an int:
+    if len(candidates) == 1:
+        return int(candidates.pop(), base=2)
+
+    # The sets of candidates with a '1' vs '0' in the next bit to consider:
+    ones = set()
+    zeroes = set()
+    candidate_count = len(candidates)
+
+    # Consider each candidate binary number and sort it into the ones or zeroes
+    #  sets based on the value in the next bit place under consideration
+    #  (which corresponds to len(prefix)):
+    while len(candidates):
+        candidate = candidates.pop()
+        if candidate[len(prefix)] == '1':
+            ones.add(candidate)
+        else:
+            zeroes.add(candidate)
+    
+    # Use the supplied operator (less-than or greater-equal) to compare
+    #  the ones and zeroes sets, selecting the ones set if the operation
+    #  supplied returns True, and the zeroes set if the operation returns
+    #  False.
+    if ones_operator(len(ones), candidate_count/2.0):
+        return filter_prefix(ones, prefix + '1', ones_operator)
+    else:
+        return filter_prefix(zeroes, prefix + '0', ones_operator)
+
 
 def main():
-    digit_totals = None
-    line_ct = 0
+    common_candidates = set(fileinput.input())
+    
+    print(filter_prefix(common_candidates.copy(), ones_operator=operator.lt) * \
+          filter_prefix(common_candidates, ones_operator=operator.ge))
 
-    for line in fileinput.input():
-        if digit_totals == None:
-            digit_totals = [0] * len(line.strip())
-        
-        for i in range(len(line.strip())):
-            if line[i] == '1':
-                digit_totals[i] += 1
-        line_ct += 1
-
-    ones = ''.join(map(lambda a: '1' if a>=line_ct/2 else '0', digit_totals))
-    zeroes = ''.join(map(lambda a: '0' if a>=line_ct/2 else '1', digit_totals))
-
-    best_zeroes = 0
-    best_zero_count = 0
-
-    best_ones = 0
-    best_one_count = 0
-
-    print(ones, zeroes)
-
-    for line in fileinput.input():
-        one_prefix = commonprefix([line.strip(), ones])
-        if len(one_prefix) > best_one_count:
-            best_ones = int(line, base=2)
-            best_one_count = len(one_prefix)
-        
-        zero_prefix = commonprefix([line.strip(), zeroes])
-        if len(zero_prefix) > best_zero_count:
-            best_zeroes = int(line, base=2)
-            best_zero_count = len(zero_prefix)
-
-    print(best_ones, best_zeroes)
-
-    print(best_ones * best_zeroes)
 
 if __name__ == '__main__':
     main()
