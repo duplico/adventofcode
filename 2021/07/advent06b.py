0ca3a80f405c3b77d6565743e162c16ea5fed518
George Louthan <duplico@dupli.co>
2021-12-04 22:36:34 -0600

    Add Python solution for 2021.07.2.

---
+++ 2021/07/advent06b.py
@@ -0,0 +1,25 @@
+import fileinput
+from collections import namedtuple
+
+def main():
+    starting_positions = fileinput.input().readline().strip().split(',')
+    starting_positions = list(map(int, starting_positions))
+
+    best_pos = 0
+    best_pos_fuel = None
+
+    for dest_pos in range(min(starting_positions), max(starting_positions)+1):
+        pos_fuel = 0
+        for src_pos in starting_positions:
+            distance = abs(src_pos - dest_pos)
+            # Standard formula for sum of consecutive natural numbers: 
+            #   sum(1..N) = (N)(N+1)/2
+            pos_fuel += int((distance * (distance+1))/2)
+        if not best_pos_fuel or pos_fuel < best_pos_fuel:
+            best_pos_fuel = pos_fuel
+            best_pos = dest_pos
+    
+    print(f"Best position is {best_pos}, using {best_pos_fuel} fuel.")
+
+if __name__ == '__main__':
+    main()
