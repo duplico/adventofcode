975320725073d407b71212ade4ff56dff552146b
George Louthan <duplico@dupli.co>
2021-12-14 13:26:29 -0600

    Fix python 2021.14.2 with an actually tractable approach.

--- 2021/14/advent14b.py
+++ 2021/14/advent14b.py
@@ -1,5 +1,5 @@
 import fileinput
-from collections import Counter
+from collections import Counter, defaultdict
 
 STEP_COUNT = 40
 
@@ -8,6 +8,7 @@ insertion_rules = dict()
 def main():
      line_input = fileinput.input()
 
+     # First line is the initial polymer
      polymer = line_input.readline().strip()
      # Throw away the blank second line
      line_input.readline()
@@ -17,25 +18,36 @@ def main():
           sandwich, meat = line.strip().split(' -> ')
           insertion_rules[sandwich] = meat
 
+     # Load up a dictionary with counts of all the pairs in the polymer:
+     polymer_pairs = defaultdict(lambda: 0)
+     for i in range(len(polymer)-1):
+          pair = polymer[i:i+2]
+          polymer_pairs[pair] += 1
+
+     # Repeatedly update the polymer_pairs dict STEP_COUNT times:
      for _ in range(STEP_COUNT):
-          print(_)
-          polymer_next = ''
-          for i in range(len(polymer)-1):
-               # Check each pair, and if it's a valid insertion,
-               #  put in the first part of the pair and the insertion.
-               # Otherwise just put in the current character in the
-               #  existing polymer chain.
-               pair = polymer[i:i+2]
-               polymer_next += pair[0]
-               if pair in insertion_rules:
-                    polymer_next += insertion_rules[pair]
-          # Add the last character, which the loop didn't pick up.
-          polymer_next += polymer[-1]
-          polymer = polymer_next
-     
+          polymer_pairs_next = defaultdict(lambda: 0)
+
+          for pair, count in polymer_pairs.items():
+               insertion = insertion_rules[pair]
+               # print(f"Replacing {count} pairs of {pair} with {pair[0]+insertion} and {insertion+pair[1]}")
+               polymer_pairs_next[pair[0]+insertion] += count
+               polymer_pairs_next[insertion+pair[1]] += count
+          polymer_pairs = polymer_pairs_next
+
+     # Now get the count of every polymer, by looking at the first item in every pair.
+     polymer_counts = defaultdict(lambda: 0)
+     for pair, count in polymer_pairs.items():
+          polymer_counts[pair[0]] += count
+     # The method above skips the very last polymer in the chain, so
+     #  add it separately:
+     polymer_counts[polymer[-1]] += 1
+
+     # Generate a list of the polymers, in the order of least to most common.
+     ordered_counts = sorted(polymer_counts.keys(), key=polymer_counts.get)
      # Our solution is count(most common letter) - count(least common)
-     polymer_counts = Counter(polymer).most_common()
-     print(polymer_counts[0][1] - polymer_counts[-1][1])
+     print(polymer_counts[ordered_counts[-1]] - polymer_counts[ordered_counts[0]])
+
      
 if __name__ == '__main__':
      main()
