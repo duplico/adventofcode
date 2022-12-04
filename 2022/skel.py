ad4c0540dfbfc01444c5f4c2e10e35582da58452
George Louthan <duplico@dupli.co>
2022-12-03 23:57:02 -0600

    Add a skeleton file.

---
+++ 2022/skel.py
@@ -0,0 +1,13 @@
+import sys
+
+def part1(filename):
+     pass
+
+def part2(filename):
+     pass
+
+if __name__ == '__main__':
+     if sys.argv[1] == '1':
+          part1(sys.argv[2])
+     else:
+          part2(sys.argv[2])
