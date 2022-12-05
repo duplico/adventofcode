b9bcfbb2add329aba7e896533d9fe5967c65810a
George Louthan <duplico@dupli.co>
2022-12-05 17:52:24 -0600

    Add day 5 part 2.

--- 2022/05/advent05.py
+++ 2022/05/advent05.py
@@ -49,7 +49,42 @@ def part1(filename):
           print(''.join(map(deque.pop, (stack[i+1] for i in range(stack_count)))))
 
 def part2(filename):
-     pass
+     stack_lines = []
+     with open(filename) as f:
+          while True:
+               line = f.readline()
+               if line.strip()[0] in string.digits:
+                    stack_count = len(line.split())
+                    break
+               stack_lines.append(line)
+
+          # Initialize a stack for each column
+          stack = dict()
+          for i in range(stack_count):
+               stack[i+1] = []
+          
+          # Load up the initial stacks with the lines we already read:
+          for line in stack_lines:
+               for col in range(stack_count):
+                    char = line[1+4*col]
+                    if char != ' ':
+                         # Add to the BEGINNING (like a reverse push)
+                         stack[col+1].insert(0, char)
+          
+          # Throw away the blank line::
+          f.readline()
+
+          # The remaining lines are our instructions.
+          for line in f.readlines():
+               matches = re.match(instruction_match_str, line)
+               count, src, dest = map(int, matches.groups())
+
+               # Manipulate the stacks according to the instructions:
+               stack[dest] += stack[src][-count:]
+               stack[src] = stack[src][:-count]
+          
+          # Finally, pop every stack and smash them together in order:
+          print(''.join(map(list.pop, (stack[i+1] for i in range(stack_count)))))
 
 if __name__ == '__main__':
      setup()
