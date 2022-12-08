737559e4f85d163d592f3c276ee2b179b7cb5596
George Louthan <duplico@dupli.co>
2022-12-07 19:14:01 -0600

    Part 2

--- 2022/06/advent.py
+++ 2022/06/advent.py
@@ -13,7 +13,13 @@ def part1(filename):
      print(window_pos)
 
 def part2(filename):
-     pass
+     with open(filename) as f:
+          stream = f.readline().strip()
+     
+     window_pos = 14
+     while len(set(stream[window_pos-14:window_pos])) != len(stream[window_pos-14:window_pos]):
+          window_pos += 1
+     print(window_pos)
 
 if __name__ == '__main__':
      setup()
