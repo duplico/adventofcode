68287577dcf5123fe6ac4344c9413ca35f753aad
George Louthan <duplico@dupli.co>
2022-12-07 19:12:57 -0600

    2022 day 6 part 1

---
+++ 2022/06/advent.py
@@ -0,0 +1,23 @@
+import sys
+
+def setup():
+     pass
+
+def part1(filename):
+     with open(filename) as f:
+          stream = f.readline().strip()
+     
+     window_pos = 4
+     while len(set(stream[window_pos-4:window_pos])) != len(stream[window_pos-4:window_pos]):
+          window_pos += 1
+     print(window_pos)
+
+def part2(filename):
+     pass
+
+if __name__ == '__main__':
+     setup()
+     if sys.argv[1] == '1':
+          part1(sys.argv[2])
+     else:
+          part2(sys.argv[2])
