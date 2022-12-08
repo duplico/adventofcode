f9dca214b5df06e522a81093c2d2225a5efb9b98
George Louthan <duplico@dupli.co>
2022-12-07 19:17:20 -0600

    Comments

--- 2022/06/advent.py
+++ 2022/06/advent.py
@@ -4,21 +4,29 @@ def setup():
      pass
 
 def part1(filename):
+     # Read the whole stream into a buffer.
      with open(filename) as f:
           stream = f.readline().strip()
      
+     # Use built-in set type cast to remove duplicates and compare size to original.
      window_pos = 4
      while len(set(stream[window_pos-4:window_pos])) != len(stream[window_pos-4:window_pos]):
           window_pos += 1
+     
+     # When did we bail?
      print(window_pos)
 
 def part2(filename):
+     # Read the whole stream into a buffer.
      with open(filename) as f:
           stream = f.readline().strip()
      
+     # Use built-in set type cast to remove duplicates and compare size to original.
      window_pos = 14
      while len(set(stream[window_pos-14:window_pos])) != len(stream[window_pos-14:window_pos]):
           window_pos += 1
+
+     # When did we bail?
      print(window_pos)
 
 if __name__ == '__main__':
