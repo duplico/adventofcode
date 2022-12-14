00ff4d64941df05933bf7cdb12ecc1b25926b740
George Louthan <duplico@dupli.co>
2022-12-13 22:48:06 -0600

    Day 10 part 2

--- 2022/10/advent.py
+++ 2022/10/advent.py
@@ -30,7 +30,34 @@ def part1(filename):
           
 
 def part2(filename):
-     pass
+     screen = [[]]
+
+     cycle = 0
+     x = 1
+
+     for line in open(filename):
+          instr = line.strip().split()
+
+          # It just so happens that an instruction takes cycles equal to the
+          #  the number of tokens it produces on a split, so take advantage
+          #  of that to increment the clock.
+          for _ in range(len(instr)):
+               xpos = cycle % 40
+               if xpos == 0:
+                    screen.append([])
+               if x in [xpos-1, xpos, xpos+1]:
+                    screen[cycle//40].append('#')
+               else:
+                    screen[cycle//40].append('.')
+
+               cycle += 1
+
+          # Handle the addx instruction.
+          if instr[0] == 'addx':
+               x += int(instr[1])
+     
+     for l in screen[:-1]:
+          print(''.join(l))
 
 if __name__ == '__main__':
      setup()
