35b5dadcb1a21a4fc1aebc215cf8a44ea813bb48
George Louthan <duplico@dupli.co>
2022-12-19 15:14:38 -0600

    Day 12 part 2

--- 2022/12/advent.py
+++ 2022/12/advent.py
@@ -8,10 +8,8 @@ def setup():
      pass
 
 def reachable1(src, dest):
-     src = src.replace('S','a')
-     src = src.replace('E','z')
-     dest = dest.replace('S','a')
-     dest = dest.replace('E','z')
+     src = src.replace('S','a').replace('E','z')
+     dest = dest.replace('S','a').replace('E','z')
      
      if src == 'z':
           return True
@@ -51,7 +49,41 @@ def part1(filename):
 
 
 def part2(filename):
-     pass
+     offsets = [ (-1, 0), (1, 0), (0, -1), (0, 1)]
+
+     terrain = nx.DiGraph()
+     candidate_starts = []
+
+     terrain_strs = []
+     for line in open(filename):
+          terrain_strs.append(line.strip().replace('S','a'))
+     
+     rows = len(terrain_strs)
+     cols = len(terrain_strs[0])
+
+     for r in range(rows):
+          for c in range(cols):
+               for dr, dc in offsets:
+                    if r+dr < 0 or r+dr >= rows or c+dc < 0 or c+dc >= cols:
+                         continue
+                    if reachable1(terrain_strs[r][c], terrain_strs[r+dr][c+dc]):
+                         terrain.add_edge(
+                              (r,c),
+                              (r+dr,c+dc)
+                         )
+                    
+                    if terrain_strs[r][c] == 'a':
+                         candidate_starts.append((r,c))
+                    elif terrain_strs[r][c] == 'E':
+                         end_pos = (r,c)
+
+     lens = []
+     for start_pos in candidate_starts:
+          try:
+               lens.append(nx.shortest_path_length(terrain, start_pos, end_pos))
+          except nx.NetworkXNoPath:
+               continue
+     print(sorted(lens)[0])
 
 if __name__ == '__main__':
      setup()
