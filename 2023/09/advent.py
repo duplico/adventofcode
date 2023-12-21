5bb413fe0ccdb6ce857c85d5ec504dbd25a75596
George Louthan <duplico@dupli.co>
2023-12-21 16:22:00 -0600

    Part 2

--- 2023/09/advent.py
+++ 2023/09/advent.py
@@ -13,6 +13,14 @@ def next_val(l : list):
           l[i] - l[i-1] for i in range(1, len(l))
      ))
 
+def prev_val(l : list):
+     if set(l) == ZERO_SET:
+          return 0
+     
+     return l[0] + prev_val(list(
+          l[i] - l[i+1] for i in range(len(l)-1)
+     ))
+
 def part1(filename):
      next_val_total = 0
 
@@ -23,7 +31,13 @@ def part1(filename):
      print(next_val_total)
 
 def part2(filename):
-     pass
+     prev_val_total = 0
+
+     for line in open(filename):
+          vals = list(map(int, line.strip().split()))
+          prev_val_total += prev_val(vals)
+
+     print(prev_val_total)
 
 if __name__ == '__main__':
      setup()
