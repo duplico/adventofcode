8b9df9b763c0435041b48231b9e1d6774d2da8f2
George Louthan <duplico@dupli.co>
2023-12-21 17:49:36 -0600

    2023 day 10 part 1

--- 2023/10/advent.py
+++ 2023/10/advent.py
@@ -1,10 +1,88 @@
 import sys
 
+pipe_connections = {
+     '|' : [(-1, 0), (1, 0)],
+     '-' : [(0, -1), (0, 1)],
+     'L' : [(-1, 0), (0, 1)],
+     'J' : [(-1, 0), (0, -1)],
+     '7' : [(0, -1), (1, 0)],
+     'F' : [(0, 1), (1, 0)],
+     '.' : [],
+     'S' : []
+}
+
+inverted = {
+     (-1, 0): (1, 0),
+     (1, 0): (-1, 0),
+     (0, -1): (0, 1),
+     (0, 1): (0, -1)
+}
+
+def connects(pipe_piece, delta):
+     return delta in pipe_connections[pipe_piece]
+
+def get_delta(pipe_piece, src_delta):
+     if src_delta not in pipe_connections[pipe_piece]:
+          return False
+
+     return pipe_connections[pipe_piece][1 if pipe_connections[pipe_piece][0] == src_delta else 0]
+
+sys.setrecursionlimit(100000) # lol.
+def loop_len(pipes: list, running_len: int, coords: tuple[int, int], src_delta: tuple[int, int]):
+     pipe_piece = pipes[coords[0]][coords[1]]
+
+     # Base case: loop completed.
+     if pipe_piece == 'S':
+          return running_len
+     
+     # Otherwise, find what our pipe connects to.
+     delta = get_delta(pipe_piece, src_delta)
+     return loop_len(
+          pipes,
+          running_len+1,
+          (coords[0] + delta[0], coords[1] + delta[1]),
+          inverted[delta]
+     )
+
 def setup():
      pass
 
 def part1(filename):
-     pass
+     starting_r = 0
+     starting_c = -1
+     pipes = []
+
+     for line in open(filename):
+          pipes.append(line.strip())
+          if 'S' in line:
+               starting_c = line.find('S')
+          if starting_c == -1:
+               starting_r += 1
+
+     # We have our starting position. We need to decide which
+     #  coordinates to use to start following our loop.
+     
+     for delta in [(1,0), (0,1), (-1, 0), (0, -1)]:
+          coords = (
+               starting_r + delta[0],
+               starting_c + delta[1]
+          )
+
+          src_delta = inverted[delta]
+          try:
+               pipe_piece = pipes[coords[0]][coords[1]]
+          except IndexError:
+               continue
+          
+          if connects(pipe_piece, src_delta):
+               break
+
+     print(loop_len(
+          pipes,
+          1,
+          coords,
+          src_delta
+     )/2)
 
 def part2(filename):
      pass
