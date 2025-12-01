6bdac8c968e0e8b221e73b690d4cd446568d7379
George Louthan <duplico@dupli.co>
2025-12-01 17:54:52 -0600

    Add part 2 solution that I don't love

--- 2025/01/advent.py
+++ 2025/01/advent.py
@@ -9,7 +9,7 @@ def setup():
 
 def common_options(f):
     @click.argument('filename', type=click.Path(exists=True))
-    @click.option('-v', '--verbose', is_flag=True, help='Enable verbose output')
+    @click.option('-v', '--verbose', count=True, help='Enable verbose output (-v, -vv)')
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
         return f(*args, **kwargs)
@@ -22,11 +22,11 @@ def cli():
 
 @cli.command(name='1')
 @common_options
-def part1(filename: str, verbose: bool):
+def part1(filename: str, verbose: int):
     pos = 50
     zeroes = 0
 
-    if verbose:
+    if verbose >= 1:
         console.print(f"The dial starts by pointing at [bold cyan]{pos}[/bold cyan].")
 
     with open(filename, 'r') as f:
@@ -37,18 +37,83 @@ def part1(filename: str, verbose: bool):
 
             pos = (pos + dir * dist) % 100
 
-            if verbose:
+            if verbose >= 1:
                 console.print(f"The dial is rotated [yellow]{line}[/yellow] to point at [bold cyan]{pos}[/bold cyan].")
             if pos == 0:
                 zeroes += 1
 
     console.print(f"The dial pointed at 0 a total of [bold green]{zeroes}[/bold green] times.")
 
+def part2_turn_dial_right(current_pos: int, distance: int, zeroes: int, verbose: int = 0) -> tuple[int, int]:
+    # When turning right, we add to the current position, so calibrate it to 0.
+    if current_pos == 100:
+        current_pos = 0
+
+    if current_pos + distance < 100:
+        new_pos = current_pos + distance
+        if verbose >= 2:
+            console.print(f" - New position without crossing 0: {new_pos}")
+        return new_pos, zeroes
+    
+    # If we're here, then we have to cross 0 at least once
+    # Turn the dial to 0.
+    distance -= (100 - current_pos)
+    current_pos = 0
+    zeroes += 1
+
+    if verbose >= 2:
+        console.print(f" - Rotated {100 - current_pos} to 0. new distance to move: {distance}, total zeroes: {zeroes}")
+
+    return part2_turn_dial_right(current_pos, distance, zeroes, verbose)
+
+def part2_turn_dial_left(current_pos: int, distance: int, zeroes: int, verbose: int = 0) -> tuple[int, int]:
+    # When turning left, we subtract from the current position, so calibrate it to 100.
+    if current_pos == 0:
+        current_pos = 100
+
+    if current_pos - distance > 0:
+        new_pos = current_pos - distance
+        if verbose >= 2:
+            console.print(f" - New position without crossing 0: {new_pos}")
+        return new_pos, zeroes
+    
+    # If we're here, then we have to cross 0 at least once
+    # Turn the dial to 0.
+    distance -= current_pos
+    zeroes += 1
+    if verbose >= 2:
+        console.print(f" - Rotated {100 - current_pos} to 0. new distance to move: {distance}, total zeroes: {zeroes}")
+    current_pos = 0
+
+    return part2_turn_dial_left(current_pos, distance, zeroes, verbose)
 
 @cli.command(name='2')
 @common_options
-def part2(filename: str, verbose: bool):
-    pass
+def part2(filename: str, verbose: int):
+    pos = 50
+    zeroes = 0
+
+    if verbose >= 1:
+        console.print(f"The dial starts by pointing at [bold cyan]{pos}[/bold cyan].")
+    
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            dir = -1 if line[0] == 'L' else 1
+            dist = int(line[1:])
+
+            if dir == 1: # right
+                pos, zeroes_to_add = part2_turn_dial_right(pos, dist, 0, verbose)
+            else: # left
+                pos, zeroes_to_add = part2_turn_dial_left(pos, dist, 0, verbose)
+            zeroes += zeroes_to_add
+
+            if pos == 100:
+                pos = 0
+
+            if verbose >= 1:
+                console.print(f"The dial is rotated [yellow]{line}[/yellow] to point at [bold cyan]{pos}[/bold cyan]; during this rotation, it points at zero [bold green]{zeroes_to_add}[/bold green] times.")
+    console.print(f"The dial pointed at 0 a total of [bold green]{zeroes}[/bold green] times.")
 
 if __name__ == '__main__':
     cli()
