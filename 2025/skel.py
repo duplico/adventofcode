fa40e369e7ae95adbae9f68a4f4f87a35021973e
George Louthan <duplico@dupli.co>
2025-12-01 17:54:44 -0600

    Update skel with verbosity

--- 2025/skel.py
+++ 2025/skel.py
@@ -9,7 +9,7 @@ def setup():
 
 def common_options(f):
     @click.argument('filename', type=click.Path(exists=True))
-    @click.option('-v', '--verbose', is_flag=True, help='Enable verbose output')
+    @click.option('-v', '--verbose', count=True, help='Enable verbose output (-v, -vv)')
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
         return f(*args, **kwargs)
@@ -22,12 +22,12 @@ def cli():
 
 @cli.command(name='1')
 @common_options
-def part1(filename: str, verbose: bool):
+def part1(filename: str, verbose: int):
     pass
 
 @cli.command(name='2')
 @common_options
-def part2(filename: str, verbose: bool):
+def part2(filename: str, verbose: int):
     pass
 
 if __name__ == '__main__':
